a06a2bea2ccb4a63ce09051ebc5164eec0f0010d
NoMercy182 <shikaridiazz@gmail.com>
2023-01-16 20:42:01 +0300

    add: lesson 21 && Cheat sheet

---
+++ Part 2/21 логические операторы/script.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const hamburger = 5;
+const fries = 0;
+
+if (hamburger && fries) {
+  console.log('ok');
+} else {
+  console.log('no');
+}
+
+console.log((hamburger && fries));
+
+
+
+console.log( NaN || 2 || undefined );  // 2
+ 
+console.log( NaN && 2 && undefined ); // NaN
+ 
+console.log( 1 && 2 && 3 ); // 3
+ 
+console.log( !1 && 2 || !3 ); // false
+ 
+console.log( 25 || null && !3 ); // 25
+ 
+console.log( NaN || null || !3 || undefined || 5); // 5
+ 
+console.log( NaN || null && !3 && undefined || 5); // 5
+ 
+console.log( 5 === 5 && 3 > 1 || 5); // true
---
+++ Сheat sheet/script.js
@@ -0,0 +1,53 @@
+'use strict';
+
+// Тернарный оператор ----------------------------------------
+
+const number = 50;
+
+(number === 50) ? console.log('Yes') : console.log('no');
+
+// Switch Case ----------------------------------------
+
+const switchNumber = 50;
+
+switch (switchNumber) {
+  case 25:
+    console.log(`no`);
+    break;
+  case 35:
+    console.log(`no`);
+    break;
+  case 50:
+    console.log(`Yes`);
+    break;
+  default:
+    console.log(`error`);
+}
+
+// Логические операторы ----------------------------------------
+
+const hamburger = 3;
+const fries = 0;
+const cola = 2;
+
+console.log(hamburger && fries && cola);
+
+console.log(1 && 0); // 0
+console.log(1 && 5); // 5
+console.log(null && 5); // null
+console.log(0 && 'dffdf'); // 0
+console.log(null && ''); // null
+
+// оператор && возвращает первое ложное значение, ну а если все прадивы - тогда последнее правдивое.
+
+console.log(1 || 0); // 1
+console.log(1 || 5); // 1
+console.log(null || 5); // 5
+console.log(0 || 'dffdf'); // 'dffdf' 
+console.log(null || ''); // ''
+
+// оператор || возвращает первое правдивое значение, Ну а если все непрадивы - тогда последнее ложное.
+
+// 5 сущностей FALSE в js:
+
+const arrFalse = [0, '', null, undefined, NaN];
